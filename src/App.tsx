199bec269d162f49553b0a89bc1b961bba9efd55
abhicism <abhicism@gmail.com>
2026-08-23 11:24:59 +0530

    used onchange event handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 // Main component for the application.
 function App() {
-    // Logs a personalized greeting to the browser console.
-    function greet(name: string) {
-        console.log(`Hello, ${name}!`);
-    }
-
-    // Render the page heading and greeting button.
-    return (
-        <div>
-            <h1>My React App</h1>
-            {/* Call greet when the user clicks this button. */}
-            <button onClick={() => greet("abhishek")}>Greet</button>
-        </div>
-    );
+  function handlechange() {
+    console.log("clicked");
+  }
+  return (
+    <div>
+    <input type ="text" onChange={handlechange} />
+    </div>
+  );
 }
 
 export default App;
