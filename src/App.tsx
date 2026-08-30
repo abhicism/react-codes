720f97762c96e2e6d37c795d7db94f5b903a3722
abhicism <abhicism@gmail.com>
2026-08-30 15:18:52 +0530

    feat: add conditional login toggle with welcome message

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import { useState } from "react";
+import {useState} from 'react';
 
 function App() {
+    //state variable to track login status 
     const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+    //function to toggle login/logout state
     function toggleLogin() {
         setIsLoggedIn(!isLoggedIn);
     }
-
     return (
         <div>
-            <h1>
-                {isLoggedIn
-                    ? "Welcome Abhishek!"
-                    : "Please Login"}
-                    //condition ? valueIfTrue : valueIfFalse
-            </h1>
-
+            <h1>Welcome to My React App</h1>
+            <p>{isLoggedIn ? 'You are logged in.' : 'You are logged out.'}</p>
             <button onClick={toggleLogin}>
-                {isLoggedIn ? "Logout" : "Login"}
+                {isLoggedIn ? 'Logout' : 'Login'}
             </button>
         </div>
     );
 }
+
+/* 
+<button onClick={toggleLogin}> creates a button ,
+onclick attribute is used to specify the function to be called when the button is clicked.
+
+{isLoggedIn ? "Logout" : "Login"}  
+This is a ternary operator inside JSX. 
+
+{isLoggedIn ? "You are logged in." : "You are logged out."}
+
+
+*/
+
 
 export default App;
