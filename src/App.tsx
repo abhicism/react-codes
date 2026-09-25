c1e8de4b4e659117728672102ed5fb108623cba6
abhicism <abhicism@gmail.com>
2026-09-25 09:50:25 +0530

    go to button app created with scroll smooth

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,27 @@
-import { useRef } from "react"; 
-// Import useRef hook from React
+import { useRef } from "react";
 
 function App() {
+  const bottomRef = useRef<HTMLDivElement>(null);
 
-    const boxRef = useRef<HTMLDivElement>(null); 
-    // Create a reference to the box div element
+  function scrollToBottom() {
+    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+  }
 
-    function handleMeasure() {
-        // Function to measure the box dimensions
+  return (
+    <div>
+      <button onClick={scrollToBottom}>Scroll to Bottom</button>
 
-        const rectangle =
-            boxRef.current?.getBoundingClientRect(); 
-        // Get the bounding rectangle of the box
+      <div style={{ height: "1500px" }}>
+        <p>Lots of content...</p>
+        <p>Lots of content...</p>
+        <p>Lots of content...</p>
+        <p>Lots of content...</p>
+        <p>Lots of content...</p>
+      </div>
 
-        console.log("Width:", rectangle?.width); 
-        // Log the width of the box
-        console.log("Height:", rectangle?.height); 
-        // Log the height of the box
-    }
-
-    return (
-        <div>
-            {/* Container div */}
-
-            <div
-                ref={boxRef} 
-                // Attach the reference to this div
-                style={{
-                    width: "300px",
-                    height: "150px",
-                    border: "2px solid black"
-                }}
-            >
-                Box
-                {/* The box element */}
-            </div>
-
-            <button onClick={handleMeasure}>
-                {/* Button triggers handleMeasure */}
-                Measure Box
-            </button>
-
-        </div>
-    );
+      <div ref={bottomRef}>Bottom of the page</div>
+    </div>
+  );
 }
 
-export default App; 
-// Export the component
+export default App;
